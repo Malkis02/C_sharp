1ec72c0fbd464dc05177a5f4767d33a144d2420d
Malkis <gantsevk@yandex.ru>
2022-12-05 01:32:18 +0500

    HomeWork3

---
+++ 3_0HW/Program.cs
@@ -0,0 +1,7 @@
+﻿string IsPalindrome(int x){
+   return x<99999&&x>10000?(x%10==x/10000&&(x%100/10)==(x/1000%10)?"палиндром":"не палиндром"):"введено не корректное число"; 
+}
+
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(IsPalindrome(num));
---
+++ 3_2HW/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadNumber(string message){
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null? 0 : int.Parse(s);
+    return a;
+}
+
+void Cube(int n){
+    for (int i = 1; i <= n; i++){
+         Console.WriteLine(Math.Pow(i,3));
+    }
+}
+
+
+int n = ReadNumber("Введите число ");
+Cube(n);
