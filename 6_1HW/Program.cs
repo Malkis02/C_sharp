6d54ae395f9be4d5d15de96f589b5d38e66b053c
Malkis <gantsevk@yandex.ru>
2022-12-13 02:34:46 +0500

    homeWork 6

---
+++ 6_1HW/Program.cs
@@ -0,0 +1,11 @@
+﻿void Intersection(double k1,double b1, double k2, double b2){
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Intersection point: {x},{y}");
+}
+
+
+Intersection(int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()));
