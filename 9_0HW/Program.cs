6c9b988344cd678a4bae4b5ecea4d1521904b8d2
Malkis <gantsevk@yandex.ru>
2022-12-22 15:58:05 +0500

    homeWork9

---
+++ 9_0HW/Program.cs
@@ -0,0 +1,15 @@
+﻿void Numbers(int n){
+    if(n == 0) return;
+    if(n%2==0)
+        Console.Write($" {n} ");
+    Numbers(n - 1);
+}
+
+int ReadNumber(string message){
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null? 0 : int.Parse(s);
+    return a;
+}
+int n = ReadNumber("Введите число ");
+Numbers(n);
